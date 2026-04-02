f67c3cf8e74a4c66a078396d214d1dcb9881b0b8
invitacion-gradjim <estudio.plantpot@gmail.com>
2026-04-02 01:52:13 -0600

    idk

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
-//IMPORTS
+// firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-//CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyD9Jl9HKMZJgwddWy8o9K5qEdXpZUOx4p0",
+  apiKey: "TU_API_KEY",
   authDomain: "invitation-dpp.firebaseapp.com",
   projectId: "invitation-dpp",
   storageBucket: "invitation-dpp.firebasestorage.app",
@@ -12,8 +12,7 @@ const firebaseConfig = {
   appId: "1:984549001849:web:cf6655c8e12a5f11ade2a8"
 };
 
-//INICIALIZAR
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-//EXPORTAR DB 
-export const db = getFirestore(app);
+export { db };
